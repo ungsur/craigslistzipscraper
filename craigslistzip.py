a98f7e589456b57dcf9f9fcac6065b1bfefaa59b
Richard Ungsunan <ungsunan@gmail.com>
2017-08-28 08:01:06 -0400

    gets links

--- craigslistzip.py
+++ craigslistzip.py
@@ -6,32 +6,24 @@ Created on Fri Aug 25 10:25:38 2017
 @author: rungsunan
 """
 baseurl = 'https://newyork.craigslist.org'
-searchurl = baseurl + '/search/zip'
+starturl = baseurl + '/search/zip'
 
 import urllib
 from bs4 import BeautifulSoup
-from pymongo import MongoClient
+#from pymongo import MongoClient
 
 
 user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
-headers={'User-Agent':user_agent,} 
+headers={'User-Agent':user_agent} 
 
-req =  urllib.request.Request(url,None,headers)
+scrapelist = [starturl]
+
+req =  urllib.request.Request(starturl,None,headers)
 resp =  urllib.request.urlopen(req)
 data = resp.read()
-
-
 soup = BeautifulSoup(data, "lxml")
 txt = (soup.prettify())
-'''
-links =  soup.find_all("a")
-for link in links:
-    print("<a href='%s'>%s</a>" %(link.get('href'), link.text))
-'''
 
-def get_pages(soupobj):
-    pagelinks = soupobj.find_all("li",{"class":"result-row"})
-    return pagelinks
 def get_page_links(soupobj):
     itemdict = {}
     g_data = soupobj.find_all("li",{"class":"result-row"})
@@ -40,6 +32,31 @@ def get_page_links(soupobj):
         itemdict[baseurl + linkinfo.get('href')] = linkinfo.text
     return itemdict
 
-firstdict = get_page_links(soup)
+all_links = {}
+def get_soup_links(soup):
+    templinks = []    
+    while (soup):
+        try :
+            soup.find('link',{"rel":"next"})["href"]
+            templinks.append(soup.find('link',{"rel":"next"})["href"])
+            newurl = soup.find('link',{"rel":"next"})["href"]
+            newreq =  urllib.request.Request(newurl,None,headers)
+            newresp =  urllib.request.urlopen(newreq)
+            newdata = newresp.read()
+            soup = BeautifulSoup(newdata, "lxml")
+        except :
+            soup = 0
+            exit
+    return templinks
+scrapelist.extend(get_soup_links(soup)) 
+
+for linkurl in scrapelist:
+    req =  urllib.request.Request(linkurl,None,headers)
+    resp =  urllib.request.urlopen(req)
+    data = resp.read()
+    soup = BeautifulSoup(data, "lxml")
+    all_links.update(get_page_links(soup))
+'''
 client = MongoClient('localhost', 27017)
 db = client.craigslist
+'''
