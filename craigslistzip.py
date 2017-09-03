2e8fedcbde25642c51d17b1464f9364fc7bba30a
Richard Ungsunan <ungsunan@gmail.com>
2017-09-03 14:21:17 -0400

    refactoring

--- craigslistzip.py
+++ craigslistzip.py
@@ -18,11 +18,14 @@ headers={'User-Agent':user_agent}
 
 scrapelist = [starturl]
 
-req =  urllib.request.Request(starturl,None,headers)
-resp =  urllib.request.urlopen(req)
-data = resp.read()
-soup = BeautifulSoup(data, "lxml")
-txt = (soup.prettify())
+def url_to_soup(url):
+    req =  urllib.request.Request(url,None,headers)
+    resp =  urllib.request.urlopen(req)
+    data = resp.read()
+    soup = BeautifulSoup(data, "lxml")
+    return soup
+
+startsoup = url_to_soup(starturl)
 
 def get_page_links(soupobj):
     itemdict = {}
@@ -33,44 +36,48 @@ def get_page_links(soupobj):
     return itemdict
 
 all_links = {}
+all_links.update(get_page_links(startsoup))
+
 def get_soup_links(soup):
     templinks = []    
     while (soup):
         try :
             soup.find('link',{"rel":"next"})["href"]
+            print(soup.find('link',{"rel":"next"})["href"])
             templinks.append(soup.find('link',{"rel":"next"})["href"])
             newurl = soup.find('link',{"rel":"next"})["href"]
-            newreq =  urllib.request.Request(newurl,None,headers)
-            newresp =  urllib.request.urlopen(newreq)
-            newdata = newresp.read()
-            soup = BeautifulSoup(newdata, "lxml")
+            soup = url_to_soup(newurl)  
         except :
             soup = 0
             exit
     return templinks
-scrapelist.extend(get_soup_links(soup)) 
 
+scrapelist.extend(get_soup_links(startsoup))
+
+for link in scrapelist:
+    all_links.update(get_page_links(url_to_soup(link)))
+'''
 for linkurl in scrapelist:
-    req =  urllib.request.Request(linkurl,None,headers)
-    resp =  urllib.request.urlopen(req)
-    data = resp.read()
-    soup = BeautifulSoup(data, "lxml")
+    print(linkurl)
+    soup = url_to_soup(linkurl)
     all_links.update(get_page_links(soup))
-    
+   
 def link_to_soup(link):
     req = urllib.request.Request(link,None,headers)
     resp = urllib.request.urlopen(req)
     data = resp.read()
     soup = BeautifulSoup(data, "lxml")
     return soup
+
 def get_link_img(link):
     print(link)
     soup = link_to_soup(link)
     return soup
-    
+   
 soupify = get_link_img(list(all_links.keys())[0])
 txt = (soupify.prettify())
-'''
+linksoup = get_link_img(list(all_links.keys())[0])
+
 client = MongoClient('localhost', 27017)
 db = client.craigslist
 '''
